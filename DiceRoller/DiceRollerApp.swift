203fd51a9e09b927dacdd42295cece90ddc943bf
gabriel.beltrame <gabriel.beltrame@totvs.com.br>
2024-06-12 17:01:00 -0300

    Initial Commit

---
+++ DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Gabriel Beltrame Silva on 12/06/24.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
